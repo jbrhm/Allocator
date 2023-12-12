49eec92fc9c71ff4a5dc5ecb4896d73e2d332229
jbrhm <134429827+jbrhm@users.noreply.github.com>
2023-12-12 15:33:31 -0500

    Heap Free Implemented

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define HEAP_CAPACITY 640000
 #define HEAP_ALLOCED_CAPACITY 640000
@@ -12,7 +13,7 @@
 
 typedef struct
 {
-    void* ptr;
+    char* ptr;
     size_t size;
 }  Heap_Chunk;
 
@@ -23,14 +24,29 @@ typedef struct
 }Heap_Chunk_List;
 
 void chunk_list_dump(const Heap_Chunk_List* list){
-    printf("The Allocated Chunks (%zu):\n", list->count);
+    printf("The Allocated Chunks (%zu) starting at %p:\n", list->count, list->chunks);
     for(int i = 0; i < list->count; i++){
-        printf(" pointer: %p, size: %zu\n", list->chunks[i].ptr, list->chunks[i].size);
+        printf(" pointer: %p, size: %zu, address of chunk: %p\n", list->chunks[i].ptr, list->chunks[i].size, &(list->chunks[i]));
     }
 }
 
+int chunk_start_compare(const void* a, const void* b){
+    const Heap_Chunk* a_chunk = a;
+    const Heap_Chunk* b_chunk = b;
+    return a_chunk->ptr - b_chunk->ptr;
+}
+
 int chunk_list_find(const Heap_Chunk_List* list, void* ptr){
-    TODO();
+    Heap_Chunk key;
+    key.ptr = ptr;
+    Heap_Chunk* result = bsearch(&key, list->chunks, list->count, sizeof(list->chunks[0]), chunk_start_compare);
+    if(result != 0){
+        assert(list->chunks <= result);
+
+        return (result - list->chunks) / sizeof(list->chunks[0]);
+    }else{
+        return -1;
+    }
 }
 
 void chunk_list_insert(Heap_Chunk_List* list, void* ptr, size_t size){
@@ -48,7 +64,14 @@ void chunk_list_insert(Heap_Chunk_List* list, void* ptr, size_t size){
 }
 
 int chunk_list_remove(Heap_Chunk_List* list, size_t index){
-    TODO();
+    //ASSERT THE INDEX IS WITHIN THE LIST OF CHUNKS
+    assert(index < list->count);
+
+    //FROM THE STARTING POINT MOVE THE FOLLOWING CHUNK INTO ITS PLACE
+    for(size_t i = index; i < list->count - 1; ++i){
+        list->chunks[i] = list->chunks[i + 1];
+    }
+    list->count -= 1;
 }
 
 char heap[HEAP_CAPACITY] = {0};
@@ -90,7 +113,20 @@ void* heap_alloc(size_t size){
 
 
 void heap_free(void* ptr){
-    TODO();
+    if(ptr != NULL){
+        //GET THE INDEX OF THE PTR IN THE ALLOCATED CHUNKS
+        const int index = chunk_list_find(&alloced_chunks, ptr);
+
+        //IF THE INDEX IS LESS THAN ZERO THEN SOMETHING WENT WRONG
+        assert(index >= 0);
+
+        //INSERT THE FREED CHUNK BACK INTO THE LIST OF FREED CHUNKS 
+        chunk_list_insert(&freed_chunks, alloced_chunks.chunks[index].ptr, alloced_chunks.chunks[index].size);
+        
+        //REMOVE THE FREED CHUNK FROM THE ALLOCATED CHUNKS
+        chunk_list_remove(&alloced_chunks, (size_t) index);
+    }
+
 }
 
 void heap_collect(){
@@ -101,11 +137,14 @@ void heap_collect(){
 int main(){
     for(int i = 0; i < 100; i++){
         void* p = heap_alloc(i);
+        printf("index %d \n", i);
+        if(i % 2 == 0){
+            heap_free(p);
+        }
     }
 
     chunk_list_dump(&alloced_chunks);
+    chunk_list_dump(&freed_chunks);
 
-
-    
     return 0;
 }
